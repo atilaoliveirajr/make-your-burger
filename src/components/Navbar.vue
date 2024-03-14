8515f7c102e3b087504463506194775921407d8b
Atila Goncalves Oliveira Junior <oliveira@artech-is.com>
2024-03-14 17:25:39 +0100

    Basic functions implemented

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav>
+      <router-link id="logo-url" to="/">
+        <img id="logo" :src="logo" :alt="alt">
+      </router-link>
+      <router-link to="/">Fazer Pedido</router-link>
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav-bar',
+  props: ['logo', 'alt'],
+};
+</script>
+
+<style scoped>
+nav {
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+nav #logo-url {
+  margin: auto auto auto 0;
+}
+
+#logo {
+  width: 40px;
+  height: 40px;
+}
+
+nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: .5s;
+}
+
+nav a:hover {
+  color: #FFF;
+}
+</style>
---
+++ src/views/PedidosView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <DashboardComp />
+  </div>
+</template>
+<script setup lang="ts">
+import DashboardComp from '@/components/Dashboard.vue';
+</script>
